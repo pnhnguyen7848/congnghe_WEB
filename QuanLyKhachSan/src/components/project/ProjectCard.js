45f363ac553e132e73f459e98963fc059d25217e
Phongtor21 <pn02102001@gmail.com>
2021-11-20 00:33:24 +0700

    Up __mocks__

--- QuanLyKhachSan/src/components/project/ProjectCard.js
+++ QuanLyKhachSan/src/components/project/ProjectCard.js
@@ -1,29 +1,13 @@
 import * as React from 'react';
-import { styled } from "@mui/material/styles";
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
 import { Link } from '@mui/material';
 
-const StyleCard = styled(Card)({
-  width: 345,
-  marginTop: 9,
-  marginBottom: 9,
-  maxHeight: 200,
-  borderRadius: 0
-});
-
 export default function ProjectCard() {
-
   return (
-    <StyleCard>
-      <CardMedia
-        tittle="Room"
-        component="img"
-        height="200"
-        image="https://www.hoteljob.vn/files/Anh-HTJ-Hong/mau-tam-trang%20tri-giuong-khach-san-dep-nhat-19.jpg"
-        alt="Paella dish"
-      />
-      <Link>Button</Link>
-    </StyleCard>
+    <Link class="about-picture" href="#">
+      <img src="https://noithatmyhouse.com/wp-content/uploads/2019/06/dien-tich-phong-khach-san-tieu-chuan_2.jpg" alt="activities1" />
+      <p class="picture-tittle">
+        Team Working
+      </p>
+    </Link>
   );
 }
