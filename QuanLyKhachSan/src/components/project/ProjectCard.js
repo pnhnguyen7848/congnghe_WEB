452534e47e7b05d094464fcda0febe05d0595784
Phongtor21 <pn02102001@gmail.com>
2021-11-20 01:20:25 +0700

    Database Projects

--- QuanLyKhachSan/src/components/project/ProjectCard.js
+++ QuanLyKhachSan/src/components/project/ProjectCard.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { Link } from '@mui/material';
 
-export default function ProjectCard() {
+export default function ProjectCard({ project, ...rest }) {
   return (
-    <Link class="about-picture" href="#">
-      <img src="https://noithatmyhouse.com/wp-content/uploads/2019/06/dien-tich-phong-khach-san-tieu-chuan_2.jpg" alt="activities1" />
+    <Link class="about-picture" href={project.url}>
+      <img src={project.image} alt="activities1" />
       <p class="picture-tittle">
-        Team Working
+        {project.title}
       </p>
     </Link>
   );
