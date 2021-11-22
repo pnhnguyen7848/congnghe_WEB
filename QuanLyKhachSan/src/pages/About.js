808aca8ec6e5bc9b7345f634920d5f99e0e5b641
pnhnguyen7848 <82748939+pnhnguyen7848@users.noreply.github.com>
2021-11-22 23:00:12 +0700

    Update About.js

--- QuanLyKhachSan/src/pages/About.js
+++ QuanLyKhachSan/src/pages/About.js
@@ -1,12 +1,32 @@
-import React from "react";
+import * as React from 'react';
+import { styled } from "@mui/material/styles";
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import AboutCard from '../components/about/AboutCard';
+import aboutMock from '../__mocks__/aboutMock';
 
-export const About = () => {
-    return (
-        <>
-             <h3>Count from MyContext</h3>
-            <h2>About me</h2>
-            <h3>Student Name: XYZ</h3>
-            <h4>Phone: 0909123456</h4>
-        </>
-    )
-};
+const StyleBox = styled(Box)({
+  padding: '1.5em',
+  // backgroundColor: '#cfe8fc',
+  height: '100vh',
+  display: 'flex',
+  justifyContent: 'space-around',
+  flexWrap: 'wrap',
+  alignContent: 'flex-start',
+  flexDirection: 'colurmn'
+});
+
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <Container >
+        <StyleBox >
+          {aboutMock.map((about) => (
+            <AboutCard about={about} />
+          ))}
+        </StyleBox>
+      </Container>
+    </React.Fragment>
+  );
+}
